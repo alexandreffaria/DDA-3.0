027bea4ad73f5b19a2d2d59e974dd6cd4d84bebb
Al F <alexandreffaria@me.com>
2021-02-21 23:39:33 -0300

    Teste de implementação de captcha

--- send_form_email.php
+++ send_form_email.php
@@ -16,16 +16,7 @@
 
   $email_subject = "Formulário do Site";
 
-  $email_body = "Nome: $nome.\n".
-                "Email: $visitor_email.\n".
-                "Empresa: $empresa.\n".
-                "Cargo: $cargo.\n".
-                "Forma de contato Preferida: $contatopref.\n".
-                "Telefone de Contato: $telefone.\n".
-                "Envio de Materiais: $materiais.\n".
-                "Política de Privacidade: $pdp.\n".
-                "Autorização de email: $newsletter.\n".
-                "Mensagem: $message.\n";
+ 
 
   $to = "contato@diadoarauto.com.br";
 
@@ -33,7 +24,46 @@
 
   $headers .= "Reply-To: $visitor_email \r\n";
 
-  mail($to,$email_subject,$email_body,$headers);
-  header("Location: email-recebido.html")
+  function reCaptcha($recaptcha){
+    $secret = "6LepXWIaAAAAANZbdaUHSVX1Gz4IaqYoBp18-VBz";
+    $ip = $_SERVER['REMOTE_ADDR'];
+  
+    $postvars = array("secret"=>$secret, "response"=>$recaptcha, "remoteip"=>$ip);
+    $url = "https://www.google.com/recaptcha/api/siteverify";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
+    $data = curl_exec($ch);
+    curl_close($ch);
+  
+    return json_decode($data, true);
+  }
+
+  $recaptcha = $_POST['g-recaptcha-response'];
+  $res = reCaptcha($recaptcha);
+
+  $email_body = "Nome: $nome.\n".
+  "Email: $visitor_email.\n".
+  "Empresa: $empresa.\n".
+  "Cargo: $cargo.\n".
+  "Forma de contato Preferida: $contatopref.\n".
+  "Telefone de Contato: $telefone.\n".
+  "Envio de Materiais: $materiais.\n".
+  "Política de Privacidade: $pdp.\n".
+  "Autorização de email: $newsletter.\n".
+  "Mensagem: $message.\n".
+  "Captcha Status:  $res\n";
+
+  if($res['success']){
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: email-recebido.html")
+  }
+  else{
+    header("Location: index.html")
+  }
+
+  
 
 ?>
